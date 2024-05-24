658a62db1334015294395624ea7aefe3c845a019
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2024-05-24 23:33:54 +0200

    Use fake Axon in IT tests

--- src/test/java/nl/robinthedev/tictactoe/TicTacToeMmoApplicationTests.java
+++ src/test/java/nl/robinthedev/tictactoe/TicTacToeMmoApplicationTests.java
@@ -1,11 +1,26 @@
 package nl.robinthedev.tictactoe;
 
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
+import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TicTacToeMmoApplicationTests.AxonTestConfig.class)
 class TicTacToeMmoApplicationTests {
 
   @Test
   void contextLoads() {}
+
+  @TestConfiguration
+  static class AxonTestConfig {
+    @Bean
+    public static EventBus createSpyEventBus() {
+      return EmbeddedEventStore.builder().storageEngine(new InMemoryEventStorageEngine()).build();
+    }
+  }
 }
